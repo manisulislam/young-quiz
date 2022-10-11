e763c9bdb17c10a179d4a410aadda7dad0d05f11
anisul islam <anisulislam980340@gmail.com>
2022-10-11 15:28:14 +0600

    complete two question

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
         <div>
-            <h4>blog start this ping</h4>
+            <div>
+                <h1>
+                1. What is the purpose of react router?
+                    
+                </h1>
+                <p className='first-question'>
+                Ans: React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+                </p>
+            </div>
+            <div>
+                <h1>
+
+                2. How context API works?
+                </h1>
+                <p>
+                Ans: The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+                React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. More information can be found on React's documentation page
+                </p>
+            </div>
+            <div></div>
         </div>
     );
 };
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import './Quiz.css'
 
 const Quiz = ({quiz}) => {
     console.log(quiz)
+    const {id, name, logo, total}= quiz;
     return (
-        <div>
-            <h3> anis</h3>
+    
+
+     
+        <div className='quiz-container'>
+            <section>
+                
+            </section>
+           
+            <div>
+            <img className='quiz-image' src={logo} alt="" />
+            <div className='info-container'>
+                <div className='name-container'>
+                <h3 >Name: {name}</h3>
+                </div>
+                <div className='total-container'>
+                <h4>Total: {total}</h4>
+
+                </div>
+            </div>
+            
+            
+            <button className='btn-one'>Start quiz</button>
+            </div>
+           
+            
         </div>
     );
 };
